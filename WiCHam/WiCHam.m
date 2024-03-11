20a227015b04fad79d156df4c571d1be22c1506e
Mattia Bruno <38449948+mbruno46@users.noreply.github.com>
2024-03-11 12:02:35 +0100

    cleaner structure

---
+++ WiCHam/WiCHam.m
@@ -0,0 +1,13 @@
+(* ::Package:: *)
+
+(* ::Section:: *)
+(*WiCHam*)
+
+
+Get["Params.m"]
+Get["AdMat.m"]
+Get["RG.m"]
+Get["WilsCoeff.m"]
+Get["RI2MS.m"]
+
+Print["WiCHam library written by Mattia Bruno, (C) 2016 - 2024"]
